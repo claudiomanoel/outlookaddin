d1aabe1060b7ba55e5c1708cb99c170a231236fe
Claudio Manoel da Silva e Sousa Neto <claudio_manoel2005@yahoo.com.br>
2019-04-05 20:14:28 -0300

    Envio de email no webservice

--- model/08-webservice_classification.py
+++ model/08-webservice_classification.py
@@ -8,9 +8,12 @@ class DataView(object):
  
     def POST(self):
         rawData = cherrypy.request.body.read(int(cherrypy.request.headers['Content-Length']))
-        b = json.loads(rawData)
+        requestData = json.loads(rawData)
+        requestId = requestData['id']
+        email = requestData['email']
+        classification = '1_1;2_3;4_1'.split(";")
 # do something with b, in this case I am returning it inside another object
-        return json.dumps({'x': 4, 'c': b})
+        return json.dumps({'id': requestId, 'res': classification})
  
 def CORS():
     cherrypy.response.headers["Access-Control-Allow-Origin"] = "*"
@@ -24,4 +27,4 @@ if __name__ == '__main__':
         }
     }
     cherrypy.tools.CORS = cherrypy.Tool('before_handler', CORS)
-    cherrypy.quickstart(DataView(), '/d/', conf)
+    cherrypy.quickstart(DataView(), '/email_classification/', conf)
