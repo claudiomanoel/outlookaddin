2341e0a70d5b87453c4eca798bce301e4f54b38c
Claudio Manoel da Silva e Sousa Neto <claudio_manoel2005@yahoo.com.br>
2019-05-22 20:54:38 -0300

    Outlook Addin: Correção na apresentação dos dados.

--- OutlookAddIn/OutlookAddIn/FormShowMetrics.cs
+++ OutlookAddIn/OutlookAddIn/FormShowMetrics.cs
@@ -36,9 +36,19 @@ namespace OutlookAddIn
             string[] v_labels = new string[10];
             float[] v_values = new float[v_labels.Count()];
 
+            v_labels[0] = "Admiration";
+            v_labels[1] = "Sympathy";
+            v_labels[2] = "Humor";
+            v_labels[3] = "Friendship";
+            v_labels[4] = "Confidentiality";
+            v_labels[5] = "Concern";
+            v_labels[6] = "Pride";
+            v_labels[7] = "Scorn";
+            v_labels[8] = "Jubilation";
+            v_labels[9] = "Hope";
+
             for (int i_Index = 0; i_Index < v_labels.Count(); i_Index++)
             {
-                v_labels[i_Index] = "ABCAFSA" + i_Index.ToString();
                 v_values[i_Index] = (v_labels.Count() * 3) - ((i_Index + 1) * 2);
             }
 
